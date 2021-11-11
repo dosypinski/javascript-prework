b870b8b9b0eed0cd6887c629b70cd1816d8fbd7e
Damian Osypinski <d.osypinski@gmail.com>
2021-11-11 20:03:14 +0100

    add score counter

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 {
-  var compScore = 0;
-  var playerScore = 0;
-  var round = 1;
+  var compScore = 0, playerScore = 0, round = 1;
   
   
   const playGame = function(playerInput) {
@@ -20,64 +18,54 @@
       }
     }
   
-    roundUp()
+
   
     function roundUp() {
       round += 1;
       document.getElementById("round").innerHTML  = 'Runda '+round;
     }
-  
-  
+    roundUp()
+
     const displayResult = function(argComputerMove, argPlayerMove) {
-      console.log('moves' + argComputerMove + argPlayerMove);
-      printMessage('Zagrałeś ' + argComputerMove + ', a ja ' + argPlayerMove);
+        printMessage('Zagrałeś ' + argComputerMove + ', a ja ' + argPlayerMove);
+        
       if( argComputerMove == argPlayerMove){
         printMessage('Remis!');
       }
-      else if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
-          printMessage('Wygrywasz!');
-          playerScore += 1;
+      else if(
+        (argComputerMove == 'kamień' && argPlayerMove == 'papier') || 
+        (argComputerMove == 'papier' && argPlayerMove == 'nożyce') || 
+        (argComputerMove == 'nożyce' && argPlayerMove == 'kamień')
+      ){
+        printMessage('Wygrywasz!');
+        playerScore++;
+        document.getElementById("playerScore").innerHTML = playerScore;
       }
-      else if(argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
-          printMessage('Przegrywasz!');
-          compScore += 1;
-      }
-      else if(argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
-          printMessage('Wygrywasz!');
-          playerScore += 1;
-      }
-      else if(argComputerMove == 'papier' && argPlayerMove == 'kamień') {
-          printMessage('Przegrywasz!');
-          compScore += 1;
-      }
-      else if(argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
-          printMessage('Wygrywasz!');
-          playerScore += 1;
-      }
-      else if(argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
-          printMessage('Przegrywasz');
-          compScore += 1;
+      else {
+        printMessage('Przegrywasz!')
+        compScore++;
+        document.getElementById("compScore").innerHTML = compScore;
       }
     }
   
     const randomNumber = Math.floor(Math.random() * 3 + 1);
   
     const computerMove = getMoveName(randomNumber);
-    printMessage('Mój ruch to: ' + computerMove);
+      printMessage('Mój ruch to: ' + computerMove);
   
     const playerMove = getMoveName(playerInput);
-    printMessage('Twój ruch to: ' + playerMove);
-    displayResult(playerMove, computerMove);
+      printMessage('Twój ruch to: ' + playerMove);
+      displayResult(playerMove, computerMove);
   }
-  document.getElementById('play-rock').addEventListener('click', function(){
-    playGame('1');
-  });
-  document.getElementById('play-paper').addEventListener('click', function(){
-    playGame('2');
-  });
-  document.getElementById('play-scissors').addEventListener('click', function(){
-    playGame('3');
-  });
-  document.getElementById("playerScore").innerHTML = playerScore;
-  document.getElementById("compScore").innerHTML = compScore;
+    document.getElementById('play-rock').addEventListener('click', function(){
+      playGame('1');
+    });
+    document.getElementById('play-paper').addEventListener('click', function(){
+      playGame('2');
+    });
+    document.getElementById('play-scissors').addEventListener('click', function(){
+      playGame('3');
+    });
+  
+
   }
